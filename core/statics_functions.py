c7a5e33688af68922b6636ff00b3b66306ba0c97
bdaniere <benjamin.daniere@gmail.com>
2019-10-11 12:11:40 +0200

    First process - Point

---
+++ core/statics_functions.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+# Python 3
+"""
+Created on thu Oct 11 11:00:00 2019
+
+@author: bdaniere
+
+"""
+import logging
+from shapely.geometry import Point
+import geopandas as gpd
+import pandas as pd
+import argparse
+import json
+import numpy as np
+
+"""
+Globals variables 
+"""
+logging.basicConfig(level=logging.INFO, format='%(asctime)s -- %(levelname)s -- %(message)s')
+
+""" Classes / methods / functions """
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("overpass_query", help="Input file contain overpass querry")
+    parser.add_argument("-o", "--output", help="shapefile output path ")
+
+    return parser.parse_args()
+
+
+def geocode_df(df, latitude_field, longitude_field, epsg):
+    """
+    Transform a DataFrame to GeoDataFrame based on x, y field
+    :type df: DataFrame
+    :type latitude_field: Series
+    :type longitude_field: Series
+    :type epsg: integer
+    :return: GeoDataFrame (epsg : epsg)
+    """
+
+    logging.info("Geocode Xls")
+
+    geometry = [Point(xy) for xy in zip(df[longitude_field], df[latitude_field])]
+    crs = {'init': 'epsg:' + str(epsg)}
+    df = df.drop(columns=[longitude_field, latitude_field])
+
+    gdf = gpd.GeoDataFrame(df, crs=crs, geometry=geometry)
+    return gdf
+
+
+def formatting_gdf_for_shp_export(gdf, output_path, output_name):
+    """ Formatting GeoDataFrame for export & export to shp
+     :type gdf: GeoDataFrame
+     :param output_path: complete path for the shapefile export
+     :param output_name: name for the shapefile export
+     """
+    logging.info('formatting & export GeoDataFrame')
+
+    for gdf_column in gdf.columns:
+        if type(gdf[gdf_column][gdf.index.min()]) in [str]:
+            gdf[gdf_column] = gdf[gdf_column].str.decode('utf-8-sig')
+        if type(gdf[gdf_column][gdf.index.min()]) == np.bool_:
+            gdf[gdf_column] = gdf[gdf_column].astype(str)
+        if type(gdf[gdf_column][gdf.index.min()]) == pd._libs.tslib.Timestamp:
+            gdf[gdf_column] = gdf[gdf_column].astype(str)
+        if type(gdf[gdf_column][gdf.index.min()]) == list:
+            gdf = gdf.drop(columns=[gdf_column])
+            continue
+        if len(gdf_column) > 10:
+            gdf = gdf.rename(columns={gdf_column: gdf_column[:10]})
+            gdf_column = gdf_column[:10]
+
+        # temporary patch for delete the list in the string field
+        for index_element in gdf[gdf_column].index:
+            if type(gdf[gdf_column][index_element]) == list:
+                gdf[gdf_column][index_element] = gdf[gdf_column][index_element][0]
+
+    gdf.to_file(output_path + "/" + output_name + '.shp')
+
+
+def clean_gdf_by_geometry(gdf):
+    """ Clean a GeoDataFrame : drop null / invalid / empty geometry """
+
+    logging.info("drop null & invalid & duplicate geometry")
+
+    # Check geometry validity
+    invalid_geometry = gdf[gdf.geometry.is_valid == False].count().max()
+    if invalid_geometry > 0:
+        gdf = gdf[gdf.geometry.is_valid == True]
+        logging.info("We found and drop {} invalid geometry".format(invalid_geometry))
+        logging.warning("these buildings will not be integrated into the PostGis output table")
+
+    # check empty geometry
+    null_geometry = gdf[gdf.geometry.is_valid == True].count().max()
+    if null_geometry > 0:
+        gdf = gdf[gdf.geometry.is_empty == False]
+
+    # Check duplicates geometry
+    unique_geometry = gdf.geometry.astype(str).nunique()
+    number_duplicate_geometry = gdf.geometry.count() - unique_geometry
+
+    if unique_geometry != gdf.geometry.count():
+        wkb_geometry = gdf["geometry"].apply(lambda geom: geom.wkb)
+        gdf = gdf.loc[wkb_geometry.drop_duplicates().index]
+
+    logging.info("We found and drop {} duplicates geometry".format(number_duplicate_geometry))
+    assert unique_geometry == gdf.geometry.count(), "Geometry problem in the input data: the deleted entity" \
+                                                    "number is greater than the duplicate entity number"
+
+    return gdf
+
+
+def create_index(gdf):
+    """ Increment index if ther are null or not_unique """
+
+    if gdf.index.isnull().sum() > 0 or gdf.index.is_unique == False:
+        gdf.index = range(1, len(gdf) + 1)
+    return gdf
--- osm_downloader.py
+++ osm_downloader.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# Python 3
 """
 Created on thu Oct 11 11:00:00 2019
 
@@ -6,13 +7,12 @@ Created on thu Oct 11 11:00:00 2019
 
 """
 
-import requests
-import geopandas as gpd
 import pandas as pd
-import json
 import logging
 import requests
 import os
+
+from core import statics_functions
 
 """
 Globals variables 
@@ -21,7 +21,7 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s -- %(levelname)s -- 
 ch_dir = os.getcwd().replace('\\', '/')
 
 # temp variable
-temp_file = "osm_rquest.txt"
+temp_file = "osm_request.txt"
 
 """ Classes / methods / functions """
 
@@ -32,7 +32,9 @@ def read_osm_request(file):
     :param file: path to the file containing the overpass request
     :return: file content
     """
+    logging.info("Read input osm_request file")
     # Rajouter des controles sur la validité de la requête
+
     with open(file, 'r') as osm_request_file:
         osm_request = osm_request_file.read()
 
@@ -48,22 +50,48 @@ def execute_overpass_request(overpass_query):
     :param overpass_query:
     :return:
     """
+    logging.info("Execute overpass query")
+
     # Rajouter des controle pour s'assurer que le resultat est non vide
     overpass_url = "https://overpass-api.de/api/interpreter"
     response = requests.get(overpass_url, params={'data': overpass_query})
-
     data = response.json()
 
     return data
 
 
+def json_result_to_gdf(osm_data):
+    """
+    Transform the overpass query result (json format) to geopandas.GeoDataFrame
+    :param osm_data: result to the OSM request (json format)
+    :return: GeoDataFrame
+    """
+    logging.info("Transform json result to GeoDataFrame")
+
+    # Transform json to pandas.DataFrame
+    osm_data_df = pd.DataFrame(osm_data['elements'])
+    osm_data_gdf = statics_functions.geocode_df(osm_data_df, 'lat', 'lon', 4326)
+
+    # Check the data quality & create basic index
+    osm_data_gdf = statics_functions.create_index(osm_data_gdf)
+    osm_data_gdf = statics_functions.clean_gdf_by_geometry(osm_data_gdf)
+
+    return osm_data_gdf
+
+
 def main(file):
-    file_content = read_osm_request(file)
+    # Read Json parameter with ArgParse argument
+    arg = statics_functions.parse_arguments()
+
+    file_content = read_osm_request(arg.overpass_query)
     osm_data = execute_overpass_request(file_content)
+    gdf_data = json_result_to_gdf(osm_data)
 
-    import pdb;
-    pdb.set_trace()
+    if arg.output is not None:
+        statics_functions.formatting_gdf_for_shp_export(gdf_data, ch_dir + "/output/", arg.output)
 
+
+""" PROCESS """
 
 if __name__ == "__main__":
     main(temp_file)
